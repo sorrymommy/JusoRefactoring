0338f5f318c6b091825112094430dad88b5d7c70
sorrymommy <sorrymommy@gmail.com>
2022-09-13 17:01:58 +0900

    클래스별 파일 분할

---
+++ JusoFinder/JusoParser.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JusoFinder
+{
+    public class JusoParser
+    {
+        public Result Parse(string content)
+        {
+            JObject o = JObject.Parse(content);
+
+            return JsonConvert.DeserializeObject<Result>(o.SelectToken("results").ToString());
+        }
+    }
+}
---
+++ JusoFinder/ParameterBuilder.cs
@@ -0,0 +1,22 @@
+﻿using JusoFinder.Data;
+using System.Collections.Specialized;
+
+namespace JusoFinder
+{
+    public class ParameterBuilder
+    {
+        public string GetQueryString(RequestParameter requestParameter)
+        {
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add("currentPage"  , requestParameter.CurrentPage.ToString() );
+            nvc.Add("countPerPage" , requestParameter.CountPerPage.ToString());
+            nvc.Add("resultType"   , "json"                                  );
+            nvc.Add("confmKey"     , requestParameter.ApiKey                 );
+            nvc.Add("keyword"      , requestParameter.Keyword                );
+
+            return $"{nvc.GetQueryString()}";
+
+        }
+
+    }
+}
