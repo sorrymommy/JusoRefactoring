6cf087bfdfb42f242d870670eaf5b8148c352b20
sorrymommy <sorrymommy@gmail.com>
2022-09-13 17:04:56 +0900

    상수정의

--- JusoFinder/ParameterBuilder.cs
+++ JusoFinder/ParameterBuilder.cs
@@ -5,14 +5,21 @@ namespace JusoFinder
 {
     public class ParameterBuilder
     {
+        private const string CURRENT_PAGE   = "currentPage";
+        private const string COUNT_PER_PAGE = "countPerPage";
+        private const string RESULT_TYPE    = "resultType";
+        private const string CONFIRM_KEY    = "confmKey";
+        private const string KEYWORD        = "keyword";
+        private const string JSON           = "json";
+
         public string GetQueryString(RequestParameter requestParameter)
         {
             NameValueCollection nvc = new NameValueCollection();
-            nvc.Add("currentPage"  , requestParameter.CurrentPage.ToString() );
-            nvc.Add("countPerPage" , requestParameter.CountPerPage.ToString());
-            nvc.Add("resultType"   , "json"                                  );
-            nvc.Add("confmKey"     , requestParameter.ApiKey                 );
-            nvc.Add("keyword"      , requestParameter.Keyword                );
+            nvc.Add(CURRENT_PAGE   , requestParameter.CurrentPage.ToString() );
+            nvc.Add(COUNT_PER_PAGE , requestParameter.CountPerPage.ToString());
+            nvc.Add(RESULT_TYPE    , JSON                                    );
+            nvc.Add(CONFIRM_KEY    , requestParameter.ApiKey                 );
+            nvc.Add(KEYWORD        , requestParameter.Keyword                );
 
             return $"{nvc.GetQueryString()}";
 
