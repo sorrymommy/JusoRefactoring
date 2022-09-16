075a684da49af12b41070ec3ff20d58264f515b2
sorrymommy <sorrymommy@gmail.com>
2022-09-17 07:51:29 +0900

    Console program changed

--- JusoConsole/Program.cs
+++ JusoConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JusoFinder;
+using JusoFinder.Data;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
@@ -7,8 +9,6 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
-using JusoFinder;
-using JusoFinder.Data;
 
 namespace JusoConsole
 {
@@ -26,39 +26,9 @@ namespace JusoConsole
             });
 
             foreach (Addr addr in result)
-
                 Console.WriteLine($"{addr.roadAddrPart1} {addr.roadAddrPart2} {addr.jibunAddr}");
-            }
-            catch (Exception e)
-                //통신 실패시 처리로직
-                Console.WriteLine(e.ToString());
-            }
 
             Console.ReadKey();
-        }
-
-        public List<Addr> FindAll(string apikey, string keyword)
-        {
-            var tempCountPerPage = 100;
-            var totalPages = GetTotalPageCount(apikey, keyword);
-            
-            List<Addr> addrs = new List<Addr>();
-
-            for (int i = 1; i <= totalPages; i++)
-            {
-                var result = Find(i, tempCountPerPage, apikey, keyword);
-                
-                result.ForEach(x => addrs.Add(x));
-            }
-
-            return addrs;
-
-        }
-        public List<Addr> Find(int currentPage, int countPerPage, string apikey, string keyword)
-        {
-            Result result = GetResponse(currentPage, countPerPage, apikey, keyword);
-
-            return result.Juso;
         }
     }
 }
